590f7239654cb29b8f10e82bb12aecac89a903ba
HayatoAbe2 <k024g0033@g.neec.ac.jp>
2025-05-19 11:30:56 +0900

    評価課題用の演出 +ライティング

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -1,20 +1,45 @@
 #include "object3d.hlsli"
 
 struct Material{
-    float32_t4 color ;
+    float32_t4 color;
+    int32_t enableLighting;
+    int useTexture;
+};
+
+struct DirectionalLight
+{
+    float32_t4 color;
+    float32_t3 direction;
+    float intensity;
 };
 
 Texture2D<float32_t4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
 
 ConstantBuffer<Material> gMaterial : register(b0);
+ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
 struct PixelShaderOutput{
     float32_t4 color : SV_TARGET0;
 };
 
 PixelShaderOutput main(VertexShaderOutput input){
     PixelShaderOutput output;
-    float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
-    output.color = gMaterial.color * textureColor;
+    
+    if (gMaterial.useTexture != 0){
+        float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
+        output.color = gMaterial.color * textureColor;
+    }
+    else{
+        output.color = gMaterial.color;
+    }
+    
+    // Lighting計算
+    if (gMaterial.enableLighting != 0)
+    {
+        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
+        output.color *= gDirectionalLight.color * cos * gDirectionalLight.intensity;
+    }
+    
     return output;
 }
